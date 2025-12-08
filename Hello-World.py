fe61b13b15c46d80b1ef6e4d094259ddca44e43a
Gustavo Yuken <eaiyukenish4.0@gmail.com>
2025-12-08 14:59:16 -0300

    testes práticos no VScode

--- Hello-World.py
+++ Hello-World.py
@@ -2,3 +2,11 @@ print('Hello World') #Dentro do VSCode
 a = 'batata'
 print(a is 'batata')
 print('a' in 'batata')
+
+def sacar (valor):
+    conta = 500
+    if conta > valor:
+        conta -= valor
+        print(f'Seu saque foi realizado valor retirado \033[0:32mR${valor}\033[m')
+
+sacar(100)
