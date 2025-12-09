6a973e24f7737d1ad6bd64dd40b13aa5852b2caf
Gustavo Yuken <eaiyukenish4.0@gmail.com>
2025-12-09 13:45:13 -0300

    Testes programa

--- Hello-World.py
+++ Hello-World.py
@@ -1,4 +1,4 @@
-print('Hello World') #Dentro do VSCode
+'''print('Hello World') #Dentro do VSCode
 a = 'batata'
 print(a is 'batata')
 print('a' in 'batata')
@@ -9,4 +9,26 @@ def sacar (valor):
         conta -= valor
         print(f'Seu saque foi realizado valor retirado \033[0:32mR${valor}\033[m')
 
-sacar(100)
+sacar(100)'''
+
+nome = 'Guanavara'
+print(nome[::-1])
+print(nome[2:7:2])
+
+lista = [1,2,3,4,5,6]
+#lista.reverse()
+print(lista)
+print(lista[::-1])
+print(sorted(lista,reverse=True))
+print(lista)
+teste = [range(10) ]
+print(teste)
+a= list('python')
+print(a)
+tupla = ('s','b','c')
+a = input('Digite um valor: ')
+tupla2 = (a,) #caso em que pode-se ocorrer um problema do interpretador não entender que é tupla
+print(f'A tupla é {tupla2}')
+a = input('Digite um valor: ')
+print(f'A tupla é {tupla2}')
+print(isinstance(tupla2,tuple))
