111772eb33c9bf57a3d004dd53a273896e74c730
Gustavo Yuken <eaiyukenish4.0@gmail.com>
2025-12-11 10:18:36 -0300

    primeiros passos na criação na modularização da função saque

---
+++ Desafios/desafio1.py
@@ -0,0 +1,49 @@
+'''Separar as funções existentes de saque, depósito e extrato em funções dedicadas.
+Novas Funções:
+Criar uma nova função para cadastrar usuário (cliente).
+Criar uma nova função para cadastrar conta bancária'''
+
+from
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "s":
+        saque(valor,saldo,numero_saques,LIMITE_SAQUES)
+
+    elif opcao == "e":
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif opcao == "q":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
